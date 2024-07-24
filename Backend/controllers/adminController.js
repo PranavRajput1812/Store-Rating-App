d973761d5d5ea9f8e4cb0134a3931b195fb51499
Pranav Rajput <pranavrajput182002@gmail.com>
2024-07-24 10:17:03 +0530

    DashBoard Data Added.

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,6 +1,7 @@
 import AppError from "../utils/errorUtils.js"
 import { configDotenv } from "dotenv"
 import store  from "../models/storeModel.js";
+import user from "../models/userModel.js";
 configDotenv();
 
 const addStore = async(req,res)=>{
@@ -19,7 +20,7 @@ const addStore = async(req,res)=>{
         Address,
         rating
     })
-    console.log(newStore);
+   // console.log(newStore);
     if(!newStore){
         return next(new AppError(500,`Store registration failed, please try again`))
     }
@@ -33,4 +34,31 @@ const addStore = async(req,res)=>{
     })
 }
 
-export {addStore};
+const dashBoardData = async (req, res) => {
+    try {
+        const users = await user.find({});
+        
+        const stores = await store.find({});
+
+        
+
+        if (!userCount) {
+            return res.status(500).json({
+                success: false,
+                message: 'Error!'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: 'User count retrieved successfully!',
+            users,
+            stores
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+export {addStore,dashBoardData};
