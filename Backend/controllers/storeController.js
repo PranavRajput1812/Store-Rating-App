d79b981caf98ef244cad2787c46a9283fb6978ab
Pranav Rajput <pranavrajput182002@gmail.com>
2024-07-24 15:17:52 +0530

    get user That rate stores

--- Backend/controllers/storeController.js
+++ Backend/controllers/storeController.js
@@ -1,3 +1,5 @@
+import user from "../models/userModel.js";
+import store from "../models/storeModel.js";
 const storeListing = async(req,res)=>{
     try {
         const stores = await store.find();
@@ -17,24 +19,35 @@ const storeListing = async(req,res)=>{
 const rateStore = async (req,res)=>{
     const { rating } = req.body;
     const userId = req.user.id;
+    console.log('userId',userId);
     try {
       const storeRate = await store.findById(req.params.storeId);
       console.log(storeRate);
       if (!storeRate) {
         return res.status(404).json({ msg: 'Store not found' });
       }
-      const userRating = storeRate.ratings.find(r => r.userId.toString() === userId);
+      const userRating = storeRate.ratings.find(ele => ele.userId.toString() === userId);
 
       if (userRating) {
         userRating.rating = rating;
       } else {
         storeRate.ratings.push({ userId, rating });
       }
-
+    //  console.log('userRating',userRating);
       storeRate.overallRating = storeRate.ratings.reduce((acc, r) => acc + r.rating, 0) / storeRate.ratings.length;
       await storeRate.save();
-
-      res.json(storeRate);
+      
+      let userSubmitedRating = await user.findById(userId);
+      if(userSubmitedRating.role=='USER'){
+          userSubmitedRating.isSumbmitedRating = true;
+          await userSubmitedRating.save();
+      }
+    
+      res.status(200).json({
+        success: true ,
+        message: 'Rating Save Successfully.',
+        storeRate
+      }) 
     
     } catch (err) {
       console.error(err.message);
@@ -76,4 +89,38 @@ const addStore = async(req,res)=>{
 }
 
 
-export {addStore,rateStore,storeListing}
+const getUserThatRateStore = async(req,res)=>{
+ // let storeId = req.params.storeId;
+  try {
+    let storeId = req.params.storeId;
+    console.log(storeId);
+    // Find the store by ID
+    let storeUser = await store.findById(storeId);
+    console.log(storeUser);
+    if (!storeUser) {
+      return res.status(404).json({ msg: 'Store not found' });
+    }
+    let averageRating = storeUser.overallRating;
+    // Extract user IDs from the ratings
+    let userIds = storeUser.ratings.map(r => r.userId);
+
+    // Find all users that have rated the store
+    let users = await user.find({ '_id': { $in: userIds } });
+
+    res.status(200).json({
+      success: true,
+      message: 'Users who rated the store retrieved successfully',
+      users,
+      averageRating
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+
+}
+
+export {addStore,rateStore,storeListing,getUserThatRateStore};
